b0303df92a173f51df71d6b44ed60ae7d3a4a115
Lauren Nixon <lauren.nixon@pcc.edu>
2021-02-06 20:52:15 -0800

    Add project comments

--- src/edu/pcc/cis/helloworld/HelloWorld.java
+++ src/edu/pcc/cis/helloworld/HelloWorld.java
@@ -2,6 +2,14 @@ package edu.pcc.cis.helloworld;
 
 import java.util.Date;
 
+/**
+ * HelloWorld class prints out "Hello, World!" to the terminal,
+ * and also prints the date.
+ *
+ * @author Lauren Allen
+ * @version 2021.02.06
+ */
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
